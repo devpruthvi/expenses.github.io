1471003d7f38caf1ffbea905dc1ecb3890154b73
Pruthvi Raj Nelluru <nellurup@amazon.com>
2021-06-18 10:24:46 +0530

    Added expenses static app

---
+++ precache-manifest.fa56a1a6361702c8f2523094f4d307b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03025b61b74adf8fba7ac0b640fcd505",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8535897ac277a29e6e63",
+    "url": "/static/css/2.7ac39a5d.chunk.css"
+  },
+  {
+    "revision": "db4a1d873eb838625fbe",
+    "url": "/static/css/main.4d4eb2ea.chunk.css"
+  },
+  {
+    "revision": "8535897ac277a29e6e63",
+    "url": "/static/js/2.d85e8bd8.chunk.js"
+  },
+  {
+    "revision": "f14083e6e3fa69b0fba330ce12367a6d",
+    "url": "/static/js/2.d85e8bd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db4a1d873eb838625fbe",
+    "url": "/static/js/main.452f750d.chunk.js"
+  },
+  {
+    "revision": "f31b34e9a7a60b9e51c0",
+    "url": "/static/js/runtime-main.e901c6d6.js"
+  }
+]);
